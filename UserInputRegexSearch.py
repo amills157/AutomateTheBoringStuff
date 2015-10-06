d2c04ed2d14c185294cceb3febbcb21059e25623
Millsy88 <alanmills88@gmail.com>
2015-10-06 18:55:14 +0100

    Update UserInputRegexSearch.py

--- UserInputRegexSearch.py
+++ UserInputRegexSearch.py
@@ -1,3 +1,8 @@
+#! python3
+
+#randomQuizGenerator.py - Creates quizzes with questions and answers in
+# random order, along with the answer key.
+
 import re
 
 test = open('C:\\MyPythonScripts\\Test Folder\\1.txt', 'r')
@@ -9,4 +14,9 @@ testRegex = re.compile(expression)
 
 mo = testRegex.search(contents)
 
-print (mo.group())
+if mo.group() != None:
+
+    print ('Match found: ' + mo.group())
+
+else:
+    print ('No match found')
