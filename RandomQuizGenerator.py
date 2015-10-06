6c68291837a76ef1a9e740543b9ab9860a4e637d
Millsy88 <alanmills88@gmail.com>
2015-10-06 18:08:39 +0100

    Update RandomQuizGenerator.py

--- RandomQuizGenerator.py
+++ RandomQuizGenerator.py
@@ -41,46 +41,42 @@ for quizNum in range(35):
     quizFile.write((' ' * 20) + 'State Capitals Quiz (Form%s)' % (quizNum +1))
     quizFile.write('\n\n')
 
-    # Shuffle the order of the states.
+  # Shuffle the order of the states.
 
     states = list(capitals.keys())
     random.shuffle(states)
 
     # Loop through all 50 states, making a question for each.
 
-for questionNum in range(50):
+    for questionNum in range(50):
 
     # Get rigth and wrong answers.
 
-    correctAnswer = capitals[states[questionNum]]
-    wrongAnswers = list(capitals.values())
-    del wrongAnswers[wrongAnswers.index(correctAnswer)]
-    wrongAnswers = random.sample(wrongAnswers, 3)
-    answerOptions = wrongAnswers + [correctAnswer]
-    random.shuffle(answerOptions)
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
 
     # Write the question and answer options to the quiz file.
 
 
-quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1,
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1,
                states[questionNum]))
-
+        quizFile.write('\n')
 # for i in range(4):
 
-for i in range(4):
-    quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
-quizFile.write('\n')
+        for i in range(4):
+            quizFile.write((' ' * 5) +' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+            quizFile.write('\n')
 
     
     # Write the answer key to a file.
 
-answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[
               answerOptions.index(correctAnswer)]))
+        answerKeyFile.write('\n')
 
 quizFile.close()
 answerKeyFile.close()
-
-quizFile.close()
-answerKeyFile.close()
-
-
